3c3dc4c51a9fdf03c9ffbe26be93b9657a372cfb
tinix84 <tinix84@gmail.com>
2026-04-04 13:33:54 +0200

    feat: add free-conv command using bisection solver
    Replaces OPTI toolbox dependency with simple bisection on the
    heat balance equation. Uses existing h_natural_convection and
    h_radiation functions from Phase 6.
    
    Closes #5

--- thermal_cli.m
+++ thermal_cli.m
@@ -65,6 +65,8 @@ function thermal_cli()
             cmd_heatsink_create(parsed);
         case 'heatsink-rth'
             cmd_heatsink_rth(parsed);
+        case 'free-conv'
+            cmd_free_conv(parsed);
         otherwise
             fprintf(2, 'Unknown command: %s\n', command);
             cli_print_help();
